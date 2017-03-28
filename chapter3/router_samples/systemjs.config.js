7cafeb73fe7d69e6d9ac0643240264bb849099f8
James Flores <james.flores@outlook.com>
2017-03-28 13:57:44 -0500

    3.6 Splitting an app into modules

--- chapter3/router_samples/systemjs.config.js
+++ chapter3/router_samples/systemjs.config.js
@@ -16,7 +16,7 @@ System.config({
         '@angular/*': { 'format': 'cjs' }
     },
     packages: {
-        'app'                              : { main: 'main-aux', defaultExtension: 'ts' },
+        'app'                              : { main: 'main-luxury', defaultExtension: 'ts' },
         'rxjs'                             : { main: 'Rx' },
         '@angular/core'                    : { main: 'core.umd.min.js' },
         '@angular/common'                  : { main: 'common.umd.min.js' },
